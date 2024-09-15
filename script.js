aeb160c657678e42a01720359d6e01701847d1fc
solomontudu <tukintudu@gmail.com>
2024-09-15 20:13:53 +0530

    adding the NASA image of the day api and made the async getNasaPictures function to get 10 images at a time

---
+++ script.js
@@ -0,0 +1,20 @@
+// NASA API
+const apiKey = "DEMO_KEY";
+const count = 10;
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// get 10 images from NASA API
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    console.log("error fetching NASA pictures", error);
+  }
+}
+
+// on load
+getNasaPictures();
